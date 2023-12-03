270b70c933ba36514b80bfa43996bfa76fbb9482
Dypa <123641757+Dypa03@users.noreply.github.com>
2023-12-03 15:15:20 +0100

    Delete Pokemonaaa directory

--- Pokemonaaa/src/main/java/com/model/entity/MoveTypeEnum.java
+++
@@ -1,7 +0,0 @@
-package com.model.entity;
-
-public enum MoveTypeEnum {
-	PHYSICAL,
-	SPECIAL,
-	STATUS
-}
--- Pokemonaaa/src/main/java/com/repository/PokemonSpecieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.model.entity.PokemonSpecie;
-
-public interface PokemonSpecieRepository extends JpaRepository<PokemonSpecie, Integer>{
-
-}
